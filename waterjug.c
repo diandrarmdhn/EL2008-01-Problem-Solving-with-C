f649c49df7fbbdac02c949e64b748fb05e0ccf8e
diandrarmdhn <71709701+diandrarmdhn@users.noreply.github.com>
2022-01-20 00:33:49 +0700

    Update waterjug.c

--- waterjug.c
+++ waterjug.c
@@ -2,12 +2,12 @@
 
 int main()
 {
-    int a = 0;
-    int b = 0;
+    int a = 0; /* teko 3 liter, kosong */
+    int b = 0; /* teko 5 liter, kosong */
     int pilihan;
     printf("Selamat datang di Game Water Jug! \nDapatkan air 4 liter dengan menggunakan teko berukuran 3 liter dan 5 liter");
 
-    while (b != 4)
+    while (b != 4)  /* program akan looping selama salah satu teko belum berisi 4 liter */
     {
         printf("\nPermainan berakhir ketika teko berisi 4 liter air \n");
         printf("\nteko 3 liter\t\t teko 5 liter\n");
@@ -20,18 +20,18 @@ int main()
         printf("5. tuangkan isi teko 3 liter ke dalam teko 5 liter\n");
         printf("6. tuangkan isi teko 5 liter ke dalam teko 3 liter\n");
         printf("Nomor instruksi yang dipilih : ");
-        scanf("%d", &pilihan);
+        scanf("%d", &pilihan); /*input instruksi yang dipilih*/
         printf("\n");
 
         if (pilihan == 1){
-            a = 3;
+            a = 3; /* mengisi teko 3 liter */
         }else if (pilihan == 2){
-            b = 5;
+            b = 5; /* mengisi teko 5 liter */
         }else if (pilihan == 3){
-            a = 0;
+            a = 0; /* mengosongkan teko 3 liter */
         }else if (pilihan == 4){
-            b = 0;
-        }else if (pilihan == 5){
+            b = 0; /* mengosongkan teko 3 liter */
+        }else if (pilihan == 5){  /* menuangkan isi teko 3 liter ke 5 liter */
             if (a > 0){
                 b = a+b;
                 if (b>5){
@@ -41,7 +41,7 @@ int main()
                     a = 0;
                 }
             }
-        }else if (pilihan == 6){
+        }else if (pilihan == 6){  /* menuangkan isi teko 5 liter ke 3 liter */
             if (b>0){
                 a = a+b;
                 if (a>3){
