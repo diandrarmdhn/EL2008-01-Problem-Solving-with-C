e98c1e7f6b39886c4cd0a739fd16e88fb97f00f8
diandrarmdhn <71709701+diandrarmdhn@users.noreply.github.com>
2022-02-10 13:27:33 +0700

    Update structarr_latihan3.c

--- structarr_latihan3.c
+++ structarr_latihan3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #define maks 5
 
 struct mhs {
@@ -7,20 +8,32 @@ struct mhs {
     float kehadiran;
 };
 
-int main(){
+int main()
+{
     int i;
+    int a;
     struct mhs arr_mhs[maks];
 
     for (i=0; i<maks; i++)
     {
-        printf("\n Masukkan data mahasiswa %d\n", i+1);
+        printf("Masukkan data mahasiswa %d\n", i+1);
 
         printf("Masukkan nama : ");
-        scanf("%s", &arr_mhs[i].nama);
+        scanf("%s", arr_mhs[i].nama);
         printf("Masukkan NIM mahasiswa : ");
         scanf("%d", &arr_mhs[i].nim);
         printf("Masukkan persentase kehadiran : ");
         scanf("%f", &arr_mhs[i].kehadiran);
-        printf("\n")
+        printf("\n");
     }
+    printf("-Mahasiswa dengan jumlah kehadiran kurang dari minimal kehadiran-\n");
+    
+    for(i=0; i<maks; i++)
+    {
+        if (arr_mhs[i].kehadiran < 80)
+        {
+            printf("Nama : %s \t NIM : %d \n", arr_mhs[i].nama, arr_mhs[i].nim);
+        }
+    }
+    return 0;
 }
